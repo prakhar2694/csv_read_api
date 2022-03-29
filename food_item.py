5fc8b52cd49b8fa0b41703d61ac28b1b228e91ba
Prakhar Yadav <prakhar2694@gmail.com>
2022-03-29 10:35:47 +0530

    dropdown cracked

--- food_item.py
+++ food_item.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 import pandas as pd
+from flask_cors import CORS
 import ast
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
 api = Api(app)
 
 class Foods(Resource):
@@ -16,10 +19,17 @@ class Foods(Resource):
 		a=food.Description
 		b=a.tolist()
 		return b
+	@app.after_request
+	def after_request(response):
+  		response.headers.add('Access-Control-Allow-Origin', '*')
+  		response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  		response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  		return response
  
     # methods go here
 
 api.add_resource(Foods, '/foods')
+
 
 if __name__ == '__main__':
     app.run()  # run our Flask app
